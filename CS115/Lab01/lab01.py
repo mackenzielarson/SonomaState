4b9d068e793460e6617746da60ed14f9e00d4d61
Mackenzie Larson <larsonma@seawolf.sonoma.edu>
2016-07-26 12:03:10 -0700

    Add files via upload

---
+++ CS115/Lab01/lab01.py
@@ -0,0 +1,12 @@
+"""
+Lab 1: This program prompts for your name and then greets you by name.
+Author: Mackenzie Larson 
+"""
+
+
+def main():
+    name = input('What is your favorite movie? ')
+    print('Ugh, ', name, ' is a terrible movie. ', sep="")
+
+
+main()
